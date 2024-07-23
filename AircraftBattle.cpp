c02a698cdc8e15c1cb0df57447972d4ae2463db3
FlagIX <2689729698@qq.com>
2024-07-23 20:40:01 +0800

    敌机移动+子弹打敌机(运行有崩溃问题)

--- AircraftBattle.cpp
+++ AircraftBattle.cpp
@@ -1,6 +1,6 @@
 ﻿#include<stdio.h>
 #include<graphics.h>
-
+#include <time.h>
 //把图片加载进程序
 IMAGE bk;//背景的图片
 IMAGE img_role[2];//玩家飞机的图片
@@ -12,8 +12,8 @@ enum MyEnum
 {
 	WIDTH = 450,//背景的高度和宽度
 	HEIGHT = 850,
-	BULLET_NUM = 15,
-	ENEMY_NUM = 10,
+	BULLET_NUM = 100,
+	ENEMY_NUM = 5,
 	BIG,
 	SMALL
 };
@@ -46,12 +46,33 @@ void Loadimg()
 	//敌机图片
 	
 	//小敌机
-	loadimage(&img_enemy[0][0], "./Resource/images/bullet1.png");
-	loadimage(&img_enemy[0][1], "./Resource/images/bullet2.png");
+	loadimage(&img_enemy[0][0], "./Resource/images/enemy1.png");
+	loadimage(&img_enemy[0][1], "./Resource/images/a0b71bfc88cf49cf9abbc42229ca9d22.png");
 	//大敌机
-	loadimage(&img_enemy[1][0], "./Resource/images/bullet1.png");
-	loadimage(&img_enemy[1][0], "./Resource/images/bullet2.png");
+	loadimage(&img_enemy[1][0], "./Resource/images/enemy3_n1.png");
+	loadimage(&img_enemy[1][1], "./Resource/images/enemy3_n2.png");
 }
+
+
+//生产飞机
+void enemyHP(int i) {
+	//大飞机和小飞机是不一样的
+	if (rand()%8)//1->8都有可能
+	{
+		enemy[i].type = SMALL;
+		enemy[i].hp = 1;
+		enemy[i].width = 10;
+		enemy[i].height = 17;
+	}
+	else
+	{
+		enemy[i].type = BIG;
+		enemy[i].hp = 3;
+		enemy[i].width = 20;//测量飞机图片
+		enemy[i].height = 25;
+	}
+}
+
 
 //玩家飞机初始化
 void playerinit() {
@@ -65,6 +86,14 @@ void playerinit() {
 		bull[i].y = 0;
 		bull[i].y = false;
 	}
+	//初始化敌机
+	for (int i = 0; i < ENEMY_NUM; i++)
+	{
+		//敌机没有
+		enemy[i].live = false;
+		//生产飞机
+		enemyHP(i);
+	}
 }
 
 //贴图
@@ -75,15 +104,35 @@ void gemeDraw() {
 	putimage(0,0,&bk);
 	//贴玩家飞机图片    去掉透明背景的二进制：NOTSRCERASE,SRCINVERT(黑色去不掉)
 	putimage(player.x, player.y, &img_role[0], NOTSRCERASE);
-	putimage(player.x, player.y, &img_role[1], SRCINVERT);
+	putimage(player.x, player.y, &img_role[1], NOTSRCERASE);
 
 	//绘制子弹
 	for (int i = 0; i < BULLET_NUM; i++)
 	{
 		if (bull[i].live)
 		{
-			putimage(bull[i].x, bull[i].y, &img_bull[0], NOTSRCERASE);
-			putimage(bull[i].x, bull[i].y, &img_bull[1], SRCINVERT);
+			putimage(bull[i].x, bull[i].y, &img_bull[1]);
+			putimage(bull[i].x, bull[i].y, &img_bull[1]);
+		}
+	}
+
+	//绘制敌机
+	for (int i = 0; i < ENEMY_NUM; i++)
+	{
+		if (enemy[i].live)
+		{
+			if (enemy[i].type = BIG)
+			{
+				//绘制大敌机
+				putimage(enemy[i].x, enemy[i].y, &img_enemy[1][0], NOTSRCERASE);
+				putimage(enemy[i].x, enemy[i].y, &img_enemy[1][1], NOTSRCERASE);
+			}
+			else
+			{
+				//绘制小敌机
+				putimage(enemy[i].x, enemy[i].y, &img_enemy[0][0], NOTSRCERASE);
+				putimage(enemy[i].x, enemy[i].y, &img_enemy[0][1], NOTSRCERASE);
+			}
 		}
 	}
 }
@@ -104,6 +153,7 @@ void createBullte() {
 	}
 }
 
+
 //子弹移动
 void BullteMove() {
 	for (int i = 0; i < BULLET_NUM; i++)
@@ -117,6 +167,39 @@ void BullteMove() {
 			}
 		}
 	}
+}
+
+//生成敌机
+void createEnemy() {
+	for (int i = 0; i < ENEMY_NUM; i++)
+	{
+		//没有敌机时候，再生成敌机
+		if (!enemy[i].live)
+		{
+			enemy[i].live = true;
+			//x,y 是随机的
+			enemy[i].x = rand() % (WIDTH-60);
+			enemy[i].y = 0;
+			break;
+		}
+	}
+}
+
+
+//敌机移动
+void EnemyMove(int speed) {
+	for (int i = 0; i < ENEMY_NUM; i++)
+	{
+		if (enemy[i].live)
+		{
+			enemy[i].y += speed;
+			if (enemy[i].y > HEIGHT)
+			{
+				enemy[i].live = false;
+			}
+		}
+	}
+
 }
 
 //玩家飞机移动  获取键盘信息：上下左右
@@ -156,6 +239,45 @@ void playerMove(int speed) {
 	t2 = GetTickCount();
 }
 
+//子弹打敌机
+void playPlance() {
+	for (int i = 0; i < ENEMY_NUM; i++)
+	{
+		if (!enemy[i].live)
+		{
+			continue;
+		}
+		for (int k = 0; i < ENEMY_NUM; k++)
+		{
+			if (bull[k].x > enemy[i].x && bull[k].x<enemy[i].x + enemy[i].width &&
+				bull[k].y>enemy[i].y && bull[k].y < enemy[i].y + enemy[i].height)
+			{
+				bull[i].live = false;
+				enemy[i].hp--;
+			}
+		}
+		if (enemy[i].hp <= 0)
+		{
+			enemy[i].live = false;
+		}
+	}
+}
+
+//定时器 
+
+bool Time(int ms,int id) {
+	static DWORD t[10];
+	if (clock() - t[id] > ms)
+	{
+		//创建飞机
+		//clock 返回当前毫秒
+		t[id] = clock();
+		return true;
+	}
+		return false;
+}
+
+
 
 //程序的入口
 int main()
@@ -174,6 +296,14 @@ int main()
 		FlushBatchDraw();
 		playerMove(3);
 		BullteMove();
+		//main方法中 都封装函数
+		if (Time(1500,0))
+		{
+			createEnemy();
+		} 
+		EnemyMove(1);
+		//子弹打敌机
+		//playPlance();
 	}
 	return 0;
 }
